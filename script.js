2c47978cb21dacb0dacc6af7e87532f366b71c6c
elvis narciso <elvisnarciso222@gmail.com>
2023-05-17 21:31:05 -0300

    index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+function clickMenu(){
+    if(itens.style.display == 'flex'){
+        itens.style.display = 'none';
+    } else {
+        itens.style.display = 'flex';
+    }
+};
+
+// let clickMenu = document.querySelector('#itens');
+// if(clickMenu.classList.)
